e88aafc2c9a09b47242fe07950f7520c478ce5d1
Lydia Kim <lydiakim47@gmail.com>
2022-11-27 13:55:33 -0800

    react app created and files moved over

---
+++ react-portfolio/src/components/NavTabs.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> 
+        <span class="navbar-brand">Lydia Kim</span>
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <a
+                href="#about"
+                onClick={() => handlePageChange('About')}
+                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                >
+                About
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                href="#portfolio"
+                onClick={() => handlePageChange('Portfolio')}
+                className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+                >
+                Portfolio
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                href="#contact"
+                onClick={() => handlePageChange('Contact')}
+                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                >
+                Contact
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                href="#resume"
+                onClick={() => handlePageChange('Resume')}
+                className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                >
+                Resume
+                </a>
+            </li>
+        </ul>
+    </nav>
+  );
+}
+
+export default NavTabs;
---
+++ react-portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
