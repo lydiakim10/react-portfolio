35f1e8aaba3698638899771274afff807e7eab90
Lydia Kim <lydiakim47@gmail.com>
2022-11-28 17:04:08 -0800

    css fix to portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -11,25 +11,25 @@ export default function Portfolio() {
       <h1>My Portfolio</h1>
       <p>
         <div class="card col-lg-4">
-            <a href='https://github.com/lydiakim10/Rocket-Poket'><img src={pokemon} alt="Rocket Pocket" title='Rocket Pocket'></img></a>
+            <a href='https://github.com/lydiakim10/Rocket-Poket'><img src={pokemon} className='cardPic' alt="Rocket Pocket" title='Rocket Pocket'></img></a>
             <div class="card-body">
                 <p class="card-text">Rocket Pocket: Card Collection Database</p>
             </div>
         </div>
         <div class="card col-lg-4">
-            <a href='https://github.com/lydiakim10/express.js-notetaker'><img src={notetaker} alt="Notetaker" title='Notetaker'></img></a>
+            <a href='https://github.com/lydiakim10/express.js-notetaker'><img src={notetaker} className='cardPic' alt="Notetaker" title='Notetaker'></img></a>
             <div class="card-body">
                 <p class="card-text">Express.js Notetaker</p>
             </div>
         </div>
         <div class="card col-lg-4">
-            <a href='https://github.com/lydiakim10/team-profile-generator'><img src={teamProfile} alt="Team Profile" title='Team Profile'></img></a>
+            <a href='https://github.com/lydiakim10/team-profile-generator'><img src={teamProfile} className='cardPic' alt="Team Profile" title='Team Profile'></img></a>
             <div class="card-body">
                 <p class="card-text">OOP: Team Profile Generator</p>
             </div>
         </div>
         <div class="card col-lg-4">
-            <a href='https://github.com/lydiakim10/project2-team1'><img src={budgetTracker} alt="Budget 2 Go" title='Budget 2 Go'></img></a>
+            <a href='https://github.com/lydiakim10/project2-team1'><img src={budgetTracker} className='cardPic' alt="Budget 2 Go" title='Budget 2 Go'></img></a>
             <div class="card-body">
                 <p class="card-text">Budget 2 Go: Monthly Budget Tracker</p>
             </div>
